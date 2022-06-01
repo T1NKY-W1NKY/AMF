4e15807befae76ce10d1daa54ef5aadb48b932cf
Thomas Mahoney <tom8mahoney@gmail.com>
2022-06-01 00:32:54 -0400

    added annotation to Notification dto

--- ApexMapFinder/src/main/java/com/example/ApexMapFinder/dto/Notification.java
+++ ApexMapFinder/src/main/java/com/example/ApexMapFinder/dto/Notification.java
@@ -1,6 +1,7 @@
 package com.example.ApexMapFinder.dto;
 
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
 import java.util.List;
@@ -12,12 +13,15 @@ public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id;
+
+    @NotNull
     String email;
 //    @ElementCollection
 //    List<String> gamemodes;
 
     //each map with start with the gamemode they are attributed to:
     //BR_KingsCanyon | A_DropOff | RBR_Olympus | RA_PhaseRunner
+    @NotNull
     @ElementCollection
     List<String> maps;
 //    Map<String, List<String>> mapList;
