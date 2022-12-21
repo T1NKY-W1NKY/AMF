3eef7bfcc52e0881ced3ca578066942e837bcd6d
T1NKY-W1NKY <tom8mahoney@gmail.com>
2022-12-20 22:40:26 -0500

    added Broken Moon option to map notifications

--- ApexMapFinder/src/main/java/com/example/ApexMapFinder/dto/MapEnum.java
+++ ApexMapFinder/src/main/java/com/example/ApexMapFinder/dto/MapEnum.java
@@ -12,10 +12,13 @@ public enum MapEnum {
     RANKED_OVERFLOW (GamemodeEnum.ARENAS_RANKED, "Overflow"),
     RANKED_PARTY_CRASHER (GamemodeEnum.ARENAS_RANKED,"Party Crasher"),
     RANKED_PHASE_RUNNER (GamemodeEnum.ARENAS_RANKED, "Phase Runner"),
+
     RANKED_STORM_POINT (GamemodeEnum.BATTLEROYALE_RANKED, "Storm Point"),
     RANKED_WORLDS_EDGE (GamemodeEnum.BATTLEROYALE_RANKED, "World's Edge"),
     RANKED_KINGS_CANYON (GamemodeEnum.BATTLEROYALE_RANKED, "Kings Canyon"),
     RANKED_OLYMPUS (GamemodeEnum.BATTLEROYALE_RANKED, "Olympus"),
+    RANKED_BROKEN_MOON (GamemodeEnum.BATTLEROYALE_RANKED, "Broken Moon"),
+
     CASUAL_DROPOFF (GamemodeEnum.ARENAS, "Drop Off"),
     CASUAL_ENCORE (GamemodeEnum.ARENAS, "Encore"),
     //Habitat is named "Habitat 4" officially
@@ -23,12 +26,14 @@ public enum MapEnum {
     CASUAL_OVERFLOW (GamemodeEnum.ARENAS, "Overflow"),
     CASUAL_PARTY_CRASHER (GamemodeEnum.ARENAS,"Party Crasher"),
     CASUAL_PHASE_RUNNER (GamemodeEnum.ARENAS, "Phase Runner"),
+
     CASUAL_STORM_POINT (GamemodeEnum.BATTLEROYALE, "Storm Point"),
     CASUAL_WORLDS_EDGE (GamemodeEnum.BATTLEROYALE, "World's Edge"),
     CASUAL_KINGS_CANYON (GamemodeEnum.BATTLEROYALE, "Kings Canyon"),
-    CASUAL_OLYMPUS (GamemodeEnum.BATTLEROYALE, "Olympus");
+    CASUAL_OLYMPUS (GamemodeEnum.BATTLEROYALE, "Olympus"),
+    CASUAL_BROKEN_MOON (GamemodeEnum.BATTLEROYALE, "Broken Moon");
 
-//    Developed when working on an enum coverter class, may be revisited... Baeldung Link: https://www.baeldung.com/jpa-persisting-enums-in-jpa
+//  Developed when working on an enum coverter class, may be revisited... Baeldung Link: https://www.baeldung.com/jpa-persisting-enums-in-jpa
 //    RANKED_DROPOFF (GamemodeEnum.ARENAS_RANKED, "Drop Off", "RDO"),
 //    RANKED_ENCORE (GamemodeEnum.ARENAS_RANKED, "Encore", "REC"),
 //    //Habitat is named "Habitat 4" officially
